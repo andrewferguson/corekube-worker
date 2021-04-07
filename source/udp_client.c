356a72ff9fae4469e0c6eb991aecb32129ba27a9
Andrew Ferguson <andrewferguson500@gmail.com>
2021-04-07 15:29:20 +0100

    Add ability to run tests from UDP listener

--- source/udp_client.c
+++ source/udp_client.c
@@ -19,7 +19,7 @@
 int PORT;
   
 // Driver code 
-int send_message(char *mme_ip, char *payload) {
+int send_message(char *mme_ip, char *payload, int numResponse) {
 
     int sockfd; 
     char buffer[MAXLINE]; 
@@ -49,28 +49,22 @@ int send_message(char *mme_ip, char *payload) {
         MSG_CONFIRM, (const struct sockaddr *) &servaddr,  
             sizeof(servaddr)); 
     d_info("Message sent"); 
-          
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE,  
-                MSG_WAITALL, (struct sockaddr *) &servaddr, 
-                &len); 
-    d_info("Received message from server"); 
-    d_print_hex(buffer, n);
+
+    for (int i = 0; i < numResponse; i++) {
+        d_info("Waiting for response %d of %d.", i, numResponse);
+        n = recvfrom(sockfd, (char *)buffer, MAXLINE,  
+                    MSG_WAITALL, (struct sockaddr *) &servaddr, 
+                    &len); 
+        d_info("Received message from server"); 
+        d_print_hex(buffer, n);
+    }
   
     close(sockfd); 
     return 0; 
 }
 
-// Driver code
-int main(int argc, char const *argv[]) {
-    if(argc != 4) {
-        printf("RUN: ./udp_client <MME_IP_ADDRESS> <PORT> [MESSAGE_NUMBER=0]\n");
-        return 1;
-    }
 
-    PORT = atoi(argv[2]);
-
-    int message_number = atoi(argv[3]);
-
+void runMessage(char * ip_address, int message_number) {
     char *S1SetupRequest =
         "0f0a0c0e"
         "00110037000004003b00080002f83900"
@@ -78,7 +72,7 @@ int main(int argc, char const *argv[]) {
         "65636f6d5f4c5445426f780040000700"
         "00004002f8390089400140";
 
-    char *InitualUEMessage =
+    char *AttachRequest =
         "0f0a0c0e"
         "000c405b00000500080003400008001a"
         "00323107417108298039000000008002"
@@ -87,7 +81,7 @@ int main(int argc, char const *argv[]) {
         "00000a00004300060002f83900010064"
         "40080002f83900e000000086400130";
 
-    char *UplinkNASTransport =
+    char *AuthenticationResponse =
         "0f0a0c0e"
         "000d4036000005000000020008000800"
         "03400008001a000c0b07530800b51ac6"
@@ -100,6 +94,12 @@ int main(int argc, char const *argv[]) {
         "048006692d001a000908475ab132f300"
         "075e006440080002f83900e000000043"
         "40060002f8390001";
+
+    char *UECapabilityInfoIndication = 
+        "0f0a0c0e"
+        "00164029000003000000020008000800"
+        "03400008004a00151400940100f01800"
+        "03089864a0c1b83b07a0f80000";
 
     char *InitialContextSetupResponse = 
         "0f0a0c0e"
@@ -115,56 +115,69 @@ int main(int argc, char const *argv[]) {
         "e00000004340060002f8390001";
 
     char *DetachRequest =
-        // the follow detach request is encapsulated
-        // within a UplinkNASTransport message, and is
-        // taken from Jon's RAN emulator
         "0f0a0c0e"
         "000d404000000500000002000100080003400001001a00161527bd9a"
         "d244020745010bf602f83900040100000001006440080002f83900e000000043"
         "00060002f8390001";
-        // an (unsupported) alternative detach request,
-        // encapsulated within an InitialUEMessage
-        // (which does not contain the MME_UE_ID, so
-        // struggles with accessing the DB to deprotect
-        // the NAS-PDU)
-        /*"000c"
-        "403e000005000800020004001a001615"
-        "1705a3e7ac0a0745010bf600f16004d0"
-        "47"
-        "00000001" // this is the TMSI
-        "004300060000f160000a00"
-        "6440080000f160070801500086400130"
-        "0000";*/
+
+    char * UEContextReleaseComplete =
+        "0f0a0c0e"
+        "20170010200002000000020003000800"
+        "03400003";
 
 
     switch (message_number) {
         case 1:
-            send_message((char *)argv[1], S1SetupRequest);
+            send_message(ip_address, S1SetupRequest, 1);
             break;
         case 2:
-            send_message((char *)argv[1], InitualUEMessage);
+            send_message(ip_address, AttachRequest, 1);
             break;
         case 3:
-            send_message((char *)argv[1], UplinkNASTransport);
+            send_message(ip_address, AuthenticationResponse, 1);
             break;
         case 4:
-            send_message((char *)argv[1], SecurityModeComplete);
+            send_message(ip_address, SecurityModeComplete, 1);
             break;
         case 5:
-            send_message((char *)argv[1], InitialContextSetupResponse);
+            send_message(ip_address, InitialContextSetupResponse, 0);
             break;
         case 6:
-            send_message((char *)argv[1], AttachComplete);
+            send_message(ip_address, UECapabilityInfoIndication, 0);
             break;
         case 7:
-            send_message((char *)argv[1], DetachRequest);
+            send_message(ip_address, AttachComplete, 0);
+            break;
+        case 8:
+            send_message(ip_address, DetachRequest, 2);
+            break;
+        case 9:
+            send_message(ip_address, UEContextReleaseComplete, 0);
             break;
         default:
-            send_message((char *)argv[1], S1SetupRequest);
-            send_message((char *)argv[1], InitualUEMessage);
-            send_message((char *)argv[1], UplinkNASTransport);
-
+            d_info("Unknown message number %d", message_number);
     }
+}
+
+// Driver code
+int main(int argc, char const *argv[]) {
+    if(argc != 4) {
+        printf("RUN: ./udp_client <MME_IP_ADDRESS> <PORT> [MESSAGE_NUMBER=0]\n");
+        return 1;
+    }
+
+    char * ip_address = (char *) argv[1];
+
+    PORT = atoi(argv[2]);
+
+    int message_number = atoi(argv[3]);
+
+    if (message_number < 0) {
+        for (int i = 1; i <= 9; i++)
+            runMessage(ip_address, i);
+        return 0;
+    }
+    runMessage(ip_address, message_number);
 
     return 0;
 }
